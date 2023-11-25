a8407171baea06d7fa81207102142020cfc62ed4
Eduardo-Prates <eduardotiadoro@gmail.com>
2023-11-25 13:28:47 -0300

    Controle Jogadas code

--- src/main/java/ControleJogadas.java
+++ src/main/java/ControleJogadas.java
@@ -1,2 +1,105 @@
-package PACKAGE_NAME;public class ControleJogadas {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class JogoTokNovo extends JFrame {
+    private Tabuleiro tabuleiro;
+    private Peca pecaSelecionada;
+    private JPanel painelPrincipal;
+    private JButton[][] botoes;
+    private JPanel painelLateral;
+    private JButton botaoEsquerda;
+    private JButton botaoDireita;
+    private JButton botaoCima;
+    private JButton botaoBaixo;
+    private JLabel erroLabel;
+
+    // Adicione as variáveis de controle dos turnos
+    private boolean turnoJogadora1;
+    private boolean movimentoTOKRealizado;
+
+    public JogoTokNovo() {
+        configurarJanela();
+        configurarBarraMenus();
+        configurarPainelPrincipal();
+        configurarPainelLateral();
+        criarTabuleiro();
+
+        // Inicialize as variáveis de controle dos turnos
+        turnoJogadora1 = false;
+        movimentoTOKRealizado = false;
+
+        // Começa o jogo com o turno da jogadora 2 (peças de baixo)
+        proximoTurno();
+
+        setVisible(true);
+    }
+
+    // ... Métodos existentes ...
+
+    private void proximoTurno() {
+        if (turnoJogadora1) {
+            // Verifica se a jogadora 1 venceu
+            if (verificarVitoriaJogadora1()) {
+                JOptionPane.showMessageDialog(this, "A jogadora 1 venceu!");
+                reiniciarJogo();
+                return;
+            }
+        } else {
+            // Verifica se a jogadora 2 venceu
+            if (verificarVitoriaJogadora2()) {
+                JOptionPane.showMessageDialog(this, "A jogadora 2 venceu!");
+                reiniciarJogo();
+                return;
+            }
+        }
+
+        // Inicializa o próximo turno
+        turnoJogadora1 = !turnoJogadora1;
+        movimentoTOKRealizado = false;
+
+        // Desseleciona todas as peças
+        deselecionarTodasAsPecas();
+
+        // Atualiza a mensagem de erro
+        erroLabel.setText("Erros serão exibidos aqui");
+
+        // Desabilita os botões de movimento até que o TOK seja movido
+        habilitarDesabilitarBotoesMovimento(false);
+
+        // Atualiza a mensagem de turno
+        if (turnoJogadora1) {
+            erroLabel.setText("Turno da Jogadora 1");
+        } else {
+            erroLabel.setText("Turno da Jogadora 2");
+        }
+    }
+
+    private boolean verificarVitoriaJogadora1() {
+        // Adicione a lógica para verificar a vitória da jogadora 1
+        // Retorna true se a jogadora 1 vencer, false caso contrário
+        return false;
+    }
+
+    private boolean verificarVitoriaJogadora2() {
+        // Adicione a lógica para verificar a vitória da jogadora 2
+        // Retorna true se a jogadora 2 vencer, false caso contrário
+        return false;
+    }
+
+    private void habilitarDesabilitarBotoesMovimento(boolean habilitar) {
+        botaoEsquerda.setEnabled(habilitar);
+        botaoDireita.setEnabled(habilitar);
+        botaoCima.setEnabled(habilitar);
+        botaoBaixo.setEnabled(habilitar);
+    }
+
+    // ... Outros métodos ...
+
+    private void reiniciarJogo() {
+        // Adicione a lógica para reiniciar o jogo
+    }
+
+    // ... Outros métodos ...
 }
